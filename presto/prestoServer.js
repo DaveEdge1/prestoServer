2e2855d647c00bed7592da4f7d4908174c3ab848
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-11-02 13:18:48 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -55,7 +55,7 @@ var translate = function (uniqueID, recon){
 	   		                                          } else if (orig == 'assign_seasonality' || orig == 'change_uncertainty'){
 									  configsOrig[orig] = false
 			                                          }
-		 	                                  } else if (orig == 'obs_annualize_months' || orig == 'annualize_proxydb_months'){
+		 	                                  } else if (orig == 'obs_annualize_months' || orig == 'annualize_proxydb_months' || orig == 'obs_regrid_nlat' || orig == 'obs_regrid_nlon'){
 								  var newVal1 = configs1.value + ''
 								  console.log("caught annualize months var")
 								  configsOrig[orig] = newVal1.split(",").map(Number).filter(x => !isNaN(x))
