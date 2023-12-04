1010d31c4b7d0318197dc682a244c20aa08a46a3
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-04 14:18:21 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -1,15 +1,15 @@
-//Docker = require('dockerode');
+Docker = require('dockerode');
 express = require("express");
 PORT = process.env.PORT || 3000
 const fs = require('fs');
-const util = require('util');
-var exec = util.promisify(require("child_process").exec);
+//const util = require('util');
+//var exec = util.promisify(require("child_process").exec);
 var archiver = require('archiver');
 var YAML = require('yaml')
 const streams = require('memory-streams')
 const path = require('path')
 var app = express()
-//var docker = new Docker({protocol:'http', host: 'localhost', port: 2375});
+var docker = new Docker({protocol:'http', host: 'localhost', port: 2375});
 //var d = new Date();
 //var timeNow = d.getTime()
 var nodemailer = require('nodemailer');
@@ -269,7 +269,7 @@ runRecon = function(uniqueID, user, domain, recon) {
 	const stderr = new streams.WritableStream()
 	var dirname = '/root/presto/userRecons/' + uniqueID + '/';
 	var dockerSuccess = countNetcdf(dirname)
-	/*
+	
 	let options = {
 		Tty: false,
 		HostConfig: {
@@ -280,7 +280,7 @@ runRecon = function(uniqueID, user, domain, recon) {
 				]	
 			}
 		}
-        */
+        /*
 	var launchText = 'docker run --rm -v ' + dirname + ':' + rparams[recon].resultsDir + ' -v ' + configLoc + ':' + rparams[recon].paramsCon + ' ' + rparams[recon].conTag
 	function startContainer(launchText) {
 	  console.log('running container...');
@@ -301,7 +301,7 @@ runRecon = function(uniqueID, user, domain, recon) {
 	}
 
 	startContainer(launchText)
-        /*
+        */
 
 	
 		docker.run(rparams[recon].conTag,
@@ -322,7 +322,7 @@ runRecon = function(uniqueID, user, domain, recon) {
 				   });
 				   sendEmail(dockerSuccess, user, domain, uniqueID, configLoc)
 				   zipIt(uniqueID)
-			   })*/
+			   })
 }
   
 prestoStartHtml = function (uniqueID, user, domain, recon) {
