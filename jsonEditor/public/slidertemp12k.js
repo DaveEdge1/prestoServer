3bc5bd330d95b552a964239d04605dee087dfe6c
DaveEdge1 <david.edge@nau.edu>
2023-12-05 20:46:35 +0000

    fix temp12k

--- jsonEditor/public/slidertemp12k.js
+++ jsonEditor/public/slidertemp12k.js
@@ -2,6 +2,7 @@
 const color1 = '#896A67'
 const color2 = '#C6C6C6'
 var mapMax = 0;
+var mapCount = '';
 function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
 	    const [from, to] = getParsed(fromInput, toInput);
 	    fillSlider(fromInput, toInput, '#C6C6C6', color1, controlSlider);
@@ -260,6 +261,9 @@ function draw() {
     if (rect.h < 0){
 	rect.h = 0
     }
+    if (rect.w > 540){
+	rect.w = 540
+    }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgba(5, 5, 5, 0.3)";
@@ -300,14 +304,16 @@ function draw() {
 			    posHandle.y = rect.y + rect.h / 2;
 			    break;
 		    }
-	var latMin = Math.round(180-(rect.y+rect.h)/3-90)
-	var latMax = Math.round(90-rect.y/3)
 	if (mapMax == 180) {
+		var latMin = Math.round(180-(rect.y+rect.h)/3-90)
+		var latMax = Math.round(90-rect.y/3)
 		var lonMin = Math.round(rect.x/3-180)
 		var lonMax = Math.round((rect.x+rect.w)/3-180)
 	} else if (mapMax == 360) {
-		var lonMin = Math.round(rect.x/3)
-		var lonMax = Math.round((rect.x+rect.w)/3)
+		var latMin = Math.round(Math.round(135-rect.y-rect.h)/1.5)
+		var latMax = Math.round(90-rect.y/1.5)
+		var lonMin = Math.round(rect.x/1.5) - 360
+		var lonMax = Math.round((rect.x+rect.w)/1.5) - 360
 	}
 
 	document.getElementById("lat_max").value = latMax
@@ -325,15 +331,21 @@ function draw() {
 
 function updateRect(maxOfMap) {
 	mapMax = maxOfMap;
+	if ((Math.round((document.getElementById("lon_max").value - document.getElementById("lon_min").value) * 1000) / 1000) > 360){
+		    document.getElementById("lon_max").value = (Math.round(Math.round(document.getElementById("lon_min").value * 1000) / 1000)+360)
+	}
 	if (mapMax == 180) {
 		var xmin = Math.round(Math.round(document.getElementById("lon_min").value) + 180)*3
 		var width = ((Math.round(document.getElementById("lon_max").value) + 180) * 3) - xmin
+		var ymax = Math.round(((90*3) - Math.round(document.getElementById("lat_max").value)*3))
+		var height = Math.round(270 - (ymax + Math.round(document.getElementById("lat_min").value)*3))
 	} else if (mapMax == 360) {
-		var xmin = Math.round(Math.round(document.getElementById("lon_min").value))*3
-		var width = ((Math.round(document.getElementById("lon_max").value)) * 3) - xmin
+		var xmin = Math.round(Math.round(Math.round(document.getElementById("lon_min").value) + 360)*1.5)
+		var width = ((Math.round(document.getElementById("lon_max").value) + 360) * 1.5) - xmin
+		var ymax = Math.round((135 - Math.round(document.getElementById("lat_max").value)*1.5))
+		var height = Math.round(135 - (ymax + Math.round(document.getElementById("lat_min").value)*1.5))
 	}
-	var ymax = Math.round(((90*3) - Math.round(document.getElementById("lat_max").value)*3))
-	var height = Math.round(270 - (ymax + Math.round(document.getElementById("lat_min").value)*3))
+
 	rect.x = xmin
 	rect.y = ymax
 	rect.h = height
@@ -364,4 +376,4 @@ const uncertainty_nensInput = document.getElementById("uncertainty_nensInput");
 uncertainty_nensSilder.oninput = () => changeInput(uncertainty_nensSilder, uncertainty_nensInput);
 uncertainty_nensInput.onchange = () => changeSlider(uncertainty_nensInput, uncertainty_nensSilder);
 fillSingleSlider(uncertainty_nensSilder)
-updateRect(360)
+updateRect(180)
