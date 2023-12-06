e49f23bea3c3d63491166b1235cee59a54d70dd7
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-06 11:07:35 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -65,15 +65,13 @@ var translateJSON = function (uniqueID, recon){
 						      }*/
 						      if (orig == 'latRange' || orig == 'lonRange' || orig == 'searchRange'){
 							      var newVal1 = configs1.value
-							      if (orig == 'searchRange') {
-								      configsOrig[orig] = newVal1.split(",").map(Number).filter(x => !isNaN(x))
-							      } else if (orig == 'latRange') {
+							      if (orig == 'latRange' || orig == 'searchRange') {
 								      for (let ii = 0; ii < 2; ii++){
-									      configsOrig[orig[ii]] = newVal1.split(",").map(Number).filter(x => !isNaN(x))[ii]
+									      configsOrig[orig[ii]] = newVal1[ii]
 								      }
 							      } else if (orig == 'lonRange') {
 							      		for (let ii = 2; ii < 4; ii++){
-										configsOrig[orig[(ii-2)]] = newVal1.split(",").map(Number).filter(x => !isNaN(x))[ii]
+										configsOrig[orig[(ii-2)]] = newVal1[ii]
 									}
 							      }
 						      } else if (orig == 'maxResolution' || orig == 'duration') {
