fdc7ce9285fbf356e1b738b4087f1e0b6d3f5c19
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-07 12:54:03 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -289,9 +289,9 @@ emailHTML = function (uniqueID, destURL, configLoc, recon) {
   }
 
         zipIt = function (uniqueID) {
-		var downloadLoc = '/root/presto/userRecons/' + uniqueID + '/' + uniqueID + '.zip'
+		//var downloadLoc = '/root/presto/userRecons/' + uniqueID + '/' + uniqueID + '.zip'
 		var source_dir = '/root/presto/userRecons/' + uniqueID
-		var output = fs.createWriteStream(downloadLoc);
+		//var output = fs.createWriteStream(downloadLoc);
 		var archive = archiver('zip');
                 output.on('close', function () {
 			console.log(archive.pointer() + ' total bytes');
@@ -300,7 +300,7 @@ emailHTML = function (uniqueID, destURL, configLoc, recon) {
 		archive.on('error', function(err){
 			throw err;
 		});
-		archive.pipe(output);
+		//archive.pipe(output);
 		archive.directory(source_dir, false);
 		//archive.directory('subdir/', 'new-subdir');
 		archive.finalize();
