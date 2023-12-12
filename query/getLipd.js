2d50128f79cce981ea36ef74e4658c7254dfd324
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-12 15:51:24 -0700

    Update getLipd.js

--- query/getLipd.js
+++ query/getLipd.js
@@ -2,10 +2,33 @@ const fs = require('fs');
 const path = require('path')
 var exec = require("child_process").exec;
 
-queryParams = function (){
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+queryParams = async function (uniqueID){
   console.log('Downloading files from LiPDverse')
-  query1 = exec('docker run --rm -v ${PWD}/output:/output -v ${PWD}/queryParams.json:/queryParams.json davidedge/lipd_webapps:queryContainer');
+  query1 = exec('docker run --rm --name ' + uniqueID + ' -v ${PWD}/output:/output -v ${PWD}/queryParams.json:/queryParams.json davidedge/lipd_webapps:queryContainer');
   query1.stdout.pipe(fs.createWriteStream('queryContainer_stdout.log'));
+  await sleep(5000)
+  return 1
+}
+
+dockerStatus = async function (uniqueID) {
+  docker_status = shelljs.exec('docker ps -a').stdout
+	  if (docker_status.search(uniqueID) == -1){
+		  console.log('no container launched!')
+	  }
+          if (docker_status.search(uniqueID) != -1){
+                console.log('awaiting removal')
+                //console.log('docker_status.search("test2") !== -1: ' + docker_status.search("test2") !== -1)
+                while (docker_status.search(uniqueID) != -1){
+			await sleep(5000)
+                        docker_status = shelljs.exec('docker ps -a').stdout
+                }
+                console.log('constainer removed')
+                return 'done'
+        }
 }
 
 findLipds = function(dir1){
@@ -17,7 +40,10 @@ findLipds = function(dir1){
 }
 
 runIt = async function (){
-  await queryParams();
+  var uniqueID = Math.floor(Math.random() * 10000);
+  console.log('uniqueID: ' + uniqueID)
+  await queryParams(uniqueID);
+  await dockerStatus(uniqueID);
   console.log(findLipds('${PWD}/output'))
 }
 
