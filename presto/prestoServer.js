6f0e35958fdf77a1de7a149f6558bc054aabaa95
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-12 09:41:40 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -350,7 +350,11 @@ runRecon = async function(uniqueID, user, domain, recon) {
 			}
 		}
         */
-	var launchText = 'docker run --rm --name ' + uniqueID + ' -v ' + dirname + ':' + reconParams(recon).resultsDir + ' -v ' + configLoc + ':' + reconParams(recon).paramsCon + ' ' + reconParams(recon).conTag
+	if (recon == 'holocene_da'){
+		var launchText = 'docker run --rm --name ' + uniqueID + ' -v ' + dirname + ':' + reconParams(recon).resultsDir + ' -v ' + configLoc + ':' + reconParams(recon).paramsCon ' -v /root/holocene_da/da_main_code.py:/da_main_code.py -v /root/holocene_da/make_basic_figures.py:/make_basic_figures.py ' + reconParams(recon).conTag
+	} else {
+		var launchText = 'docker run --rm --name ' + uniqueID + ' -v ' + dirname + ':' + reconParams(recon).resultsDir + ' -v ' + configLoc + ':' + reconParams(recon).paramsCon + ' ' + reconParams(recon).conTag
+	}
 	//var launchText = 'docker run --rm --name ' + uniqueID + ' -v ' + dirname + ':' + reconParams(recon).resultsDir + ' ' + reconParams(recon).conTag
 	console.log('here1')
 	async function startContainer(launchText) {
