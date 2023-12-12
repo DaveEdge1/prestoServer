118f15c5d51a617669273b940a41ce56b27e518d
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-12 15:35:43 -0700

    Update getLipd.js

--- query/getLipd.js
+++ query/getLipd.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const path = require('path')
 var exec = require("child_process").exec;
 
 print('Downloading files from LiPDverse')
@@ -8,3 +8,13 @@ query1 = exec('docker run --rm -v ${PWD}/output:/output -v ${PWD}/queryParams.js
 
 query1.stdout.pipe(fs.createWriteStream('queryContainer_stdout.log'));
 
+findLipds = function(dir1){
+  var lipdCount = fs.readdir(dir1, (err, files) => {
+    files.filter(f => path.extname(f).toLowerCase() === '.lpd').length
+    //console.log(result)
+});
+  return lipdCount;
+}
+
+
+print(findLipds(${PWD}/output))
