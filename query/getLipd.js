652a39f6760d45708ad49e90f47d270afa4e97c8
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-12 15:20:00 -0700

    Update getLipd.js

--- query/getLipd.js
+++ query/getLipd.js
@@ -1,23 +1,10 @@
 const fs = require('fs');
 
 var exec = require("child_process").exec;
-query1 = exec('docker run --rm -v ${PWD}/output:/output -v ${PWD}/queryParams.json:/queryParams.json davidedge/lipd_webapps:queryContainer');
 
-//var myFile = fs.createWriteStream("queryContainer_stdout.log");
+print('Downloading files from LiPDverse')
+
+query1 = exec('docker run --rm -v ${PWD}/output:/output -v ${PWD}/queryParams.json:/queryParams.json davidedge/lipd_webapps:queryContainer');
 
 query1.stdout.pipe(fs.createWriteStream('queryContainer_stdout.log'));
 
-//run query container from json
-/*
-exec("docker run --rm -v ${PWD}/output:/output -v ${PWD}/queryParams.json:/queryParams.json davidedge/lipd_webapps:queryContainer", (error, stdout, stderr) => {
-	    if (error) {
-		            console.log(`error: ${error.message}`);
-		            return;
-		        }
-	    if (stderr) {
-		            console.log(`stderr: ${stderr}`);
-		            return;
-		        }
-	    console.log(`stdout: ${stdout}`);
-});
-*/
