dfbfb0a2186a47ac1d296d9a6a5e6ea3e284ee01
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-15 12:59:27 -0700

    Update queryServer.js

--- query/queryServer.js
+++ query/queryServer.js
@@ -1,5 +1,8 @@
 PORT = process.env.PORT || 3006
 
+var express = require('express'),
+    app = express()
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
