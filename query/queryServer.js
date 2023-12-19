ed7cec0736192871ed3ef927b5e0166fa2db96b2
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-19 15:21:28 -0700

    Update queryServer.js

--- query/queryServer.js
+++ query/queryServer.js
@@ -19,6 +19,16 @@ app.get('/queryTable', function (req, res) {
 	        res.sendFile(path.join(__dirname, 'query.html'))
 });
 */
+app.post('/lipdVerse', function(req, res) {
+        //console.log(userInfo)
+	//console.log(req.body)
+	//var d = new Date();
+	//var timeNow = function() { return("" + d.getTime() + Math.round(Math.random()*10000))}
+	//var downloadPath = writeConfigs(req.query.recon, req.query.user, req.query.domain, req.body, req.query.uniqueID)
+	res.send(req.body)
+	//res.download(writeConfigs(userInfo.recon, userInfo.parsedUser, userInfo.parsedDomain, req.body))
+});
+
 app.listen(PORT, function () {
     console.log(`Express server listening on port ${PORT}`)
   })
