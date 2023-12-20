ba7493a13a192df73ea686f82121fd9fc4b397ce
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2023-12-20 14:31:39 -0700

    Update queryDB.js

--- query/queryDB.js
+++ query/queryDB.js
@@ -42,7 +42,7 @@ app.get('/', function (req, res) {
 	  console.log("Connected!");
 	  con.query("SELECT * FROM query WHERE " + buildQstring(req.query.archiveType) + ";", function (err, result, fields) {
 		      if (err) throw err;
-		      res.json(JSON.stringify(result));
+		      res.json(result);
 		    });
    });
 });
