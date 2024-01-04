a2906ab52f8e351cb86f02c1f11dbbab7a2f279c
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-01-04 11:12:13 -0700

    Update queryDB.js

--- query/queryDB.js
+++ query/queryDB.js
@@ -23,7 +23,7 @@ var con = mysql.createPool({
 });
 
 function buildQstring(qs){
-	Object.entries(qs).forEach(([key, value]) => {
+	qs.forEach(([key, value]) => {
 		console.log(`${key}: ${value}`)
 		const words = value.split(',');
 		var outString = key + ' ='
