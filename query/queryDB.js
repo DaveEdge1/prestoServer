afca18ca3d3619f5273a1d699d17a21614cfba34
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-01-04 10:29:30 -0700

    Update queryDB.js

--- query/queryDB.js
+++ query/queryDB.js
@@ -42,7 +42,7 @@ app.get('/', function (req, res, next) {
    con.getConnection(function(err) {
 	  if (err) throw err;
 	  console.log("Connected!");
-	   console.log(req.query[0]);
+	   console.log(Object.keys(req.query));
 	  con.query("SELECT * FROM query WHERE " + buildQstring(req.query.archiveType) + ";", function (err, result, fields) {
 		      if (err) throw err;
 		      res.status(200).json(result);
