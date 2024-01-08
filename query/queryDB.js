b1a8e13d97f0f92ad74d1ec9077d4efce5a5fb74
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-01-08 10:48:24 -0700

    Update queryDB.js

--- query/queryDB.js
+++ query/queryDB.js
@@ -52,7 +52,7 @@ app.get('/', function (req, res, next) {
    con.getConnection(function(err) {
 	  if (err) throw err;
 	  console.log("Connected!");
-	  con.query("SELECT * FROM query WHERE geo_latitude = '$geo_latitude' AND geo_longitude = '$geo_longitude'" + buildQstring(req.query) + ";", function (err, result, fields) {
+	  con.query("SELECT geo_latitude FROM query" + buildQstring(req.query) + ";", function (err, result, fields) {
 		      if (err) throw err;
 		      res.status(200).json(result);
 		    });
