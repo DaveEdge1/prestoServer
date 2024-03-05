d73a0dea598297a425bf211c421033907799387a
DaveEdge1 <david.edge@nau.edu>
2024-03-05 19:08:45 +0000

    testing viz server

--- viz/viz.js
+++ viz/viz.js
@@ -6,6 +6,7 @@ var path = require('path')
 var app = express();
 
 setPage = function(dir1){
+	app.use(express.static(dir1 + 'public'));
 	var html_name = dir1 + 'visualizer.html';
 	return html_name;
 }
@@ -14,8 +15,6 @@ setPage = function(dir1){
 app.get("/:reconID", (req, res) => {
 	res.sendFile(setPage("/root/presto/userRecons/" + req.params.reconID + '/viz/'));
 })
-
-app.use(express.static('./assets'));
 
 app.listen(PORT, function () {
 	  console.log(`Express server listening on port ${PORT}`)
