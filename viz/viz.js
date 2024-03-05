3dc5f0f6bcf35f4ddfbc72f1d04e718b256782c9
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-03-05 11:56:45 -0700

    Update viz.js

--- viz/viz.js
+++ viz/viz.js
@@ -6,7 +6,6 @@ var path = require('path')
 var app = express();
 
 setPage = function(dir1){
-	app.use(express.static(dir1 + 'assets'));
 	var html_name = dir1 + 'visualizer.html';
 	return html_name;
 }
@@ -15,6 +14,8 @@ setPage = function(dir1){
 app.get("/:reconID", (req, res) => {
 	res.sendFile(setPage("/root/presto/userRecons/" + req.params.reconID + '/viz/'));
 })
+
+app.use(express.static('./assets'));
 
 app.listen(PORT, function () {
 	  console.log(`Express server listening on port ${PORT}`)
