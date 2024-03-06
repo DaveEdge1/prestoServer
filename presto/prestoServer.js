94d0dd6c702c506a86e4de21aabf9f13388618c8
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-03-06 09:53:01 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -212,6 +212,12 @@ updateParams = function (uniqueID, recon){
 	
 }
 
+function printProgress(progress){
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    process.stdout.write(progress + '%');
+}
+
 dockerStatus = async function (uniqueID) {
   docker_status = shelljs.exec('docker ps -a').stdout
 	  if (docker_status.search(uniqueID) == -1){
@@ -341,7 +347,10 @@ vizStatus = async function (uniqueID) {
         			break;
     			}
                         viz_status = fs.existsSync("/root/presto/userRecons/"+uniqueID+"/viz/visualizer.html")
-			console.log("time elapsed: " + timeElapsed)
+			var minutes = Math.floor(timeElapsed/60)
+			var seconds = timeElapsed - minutes * 60;
+			printProgress("time elapsed: " + minutes + ":" + seconds)
+			//console.log("time elapsed: " + minutes + ":" + seconds)
                 }
                 console.log('viz complete')
                 return 'done'
