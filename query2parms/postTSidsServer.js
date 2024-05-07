a70135c828fa4c983d92cb40d797c9ba3961fdba
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-05-07 11:34:26 -0700

    Update postTSidsServer.js

--- query2parms/postTSidsServer.js
+++ query2parms/postTSidsServer.js
@@ -52,10 +52,10 @@ var writeJSON = function(TSids, uniqueID){
 
 app.post('/', function(req, res) {
 	console.log('ping');
-	res.sendStatus(newStatus(req.body.TSids, req.body.uniqueID));
+	res.sendStatus(newStatus(req.body.TSIDs, req.body.uniqueID));
 	
-	if (newStatus(req.body.TSids, req.body.uniqueID) == 200){	
-		writeJSON(req.body.TSids, req.body.uniqueID);
+	if (newStatus(req.body.TSIDs, req.body.uniqueID) == 200){	
+		writeJSON(req.body.TSIDs, req.body.uniqueID);
 	};
 });
 
