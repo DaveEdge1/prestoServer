cdccdcccfb94815bf1e6ca4857c6f066c8c09b6e
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-07-09 12:27:38 -0700

    Update html2javascript.js

--- query/html2javascript.js
+++ query/html2javascript.js
@@ -20,7 +20,7 @@ function editHtmlFile(inputFilePath, outputFilePath) {
 
     rl.on('line', (line) => {
         // Transform the line using the provided callback
-        const editedLine = "'" + line "\n'";
+        const editedLine = "'" + line + "\n'";
         outputStream.write(editedLine);
     });
 
