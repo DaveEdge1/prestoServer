4b890be6e3146960267619490ba67bdf9cdc0cf2
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-07-09 14:02:50 -0700

    Update writeQueryForm.js

--- query/writeQueryForm.js
+++ query/writeQueryForm.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var YAML = require('yaml')
 var bodyParser = require('body-parser')
 const prompt = require('prompt-sync')();
-require('./index.js');
+import htmlString from './index.js';
 
 reconTitle = function(recon){
 	var reconTitles = fs.readFileSync("/root/presto/jsonEditor/reconTitles.json")
