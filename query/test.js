16db50b7c03dee334853f93c63b41e46aef44102
DaveEdge1 <david.edge@nau.edu>
2024-07-09 20:19:15 +0000

    final test

--- query/test.js
+++ query/test.js
@@ -1,14 +1,1 @@
-+ '<!DOCTYPE html>
-'+ '<html>
-'+ '<head>
-'+ '    <meta charset="utf-8" />
-'+ '    <title>Autocomplete Lipdverse Query</title>
-'+ '    <meta name="viewport" content="width=device-width, initial-scale=1">
-'+ '    <meta name="robots" content="noindex">
-'+ ' <link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
-'+ '     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-'+ '        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-'+ '        crossorigin="" />
-'+ '	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-'+ '	<link rel="stylesheet" href="/style.default.css" id="theme-stylesheet">
-'
+'<!DOCTYPE html>\n' +'<html>\n' +'<head>\n' +'    <meta charset="utf-8" />\n' +'    <title>Autocomplete Lipdverse Query</title>\n' +'    <meta name="viewport" content="width=device-width, initial-scale=1">\n' +'    <meta name="robots" content="noindex">\n' +' <link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">\n' +'     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"\n' +'        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="\n' +'        crossorigin="" />\n' +'	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">\n' +'	<link rel="stylesheet" href="/style.default.css" id="theme-stylesheet">\n' +
