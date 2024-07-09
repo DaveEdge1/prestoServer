84064627c33e3f21c42231ab2ba46399b2a8e518
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-07-09 13:22:49 -0700

    Update temp12k.js

--- query/temp12k.js
+++ query/temp12k.js
@@ -1,4 +1,4 @@
-'<!DOCTYPE html>\n' +
+var htmlString = '<!DOCTYPE html>\n' +
 '<html>\n' +
 '<head>\n' +
 '    <meta charset="utf-8" />\n' +
@@ -1803,4 +1803,4 @@
 '});\n' +
 'updateBoundingBox();\n' +
 '</script>\n' +
-'</html>\n' +
+'</html>\n'
