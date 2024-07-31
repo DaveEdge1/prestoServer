ec78f5e7da0a068c93c19aceba07194c022feb83
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-07-31 15:04:01 -0500

    Update Rserver.js

--- getLipds/Rserver.js
+++ getLipds/Rserver.js
@@ -104,10 +104,28 @@ pickleEm = function(path1){
 app.post('/lipds', function(req, res) {
 	res.sendStatus(newStatus(req.body.TSIDs, req.body.uniqueID, req.body.language));
 	
-	if (newStatus(req.body.TSIDs, req.body.uniqueID, req.body.language) == 200){	
-		rspawn1(req.body.TSIDs, req.body.uniqueID, req.body.language).then(reso => {
+	if (newStatus(req.body.TSIDs, req.body.uniqueID, req.body.language) == 200){
+			fs.writeFile('TSIDs.txt', JSON.stringify(req.body.TSIDs), (err) => {
+				  if (err)
+					    console.log(err);
+				  else {
+					      console.log("File written successfully\n");
+					      console.log("The written has the following contents:");
+					      console.log(fs.readFileSync(path2, "utf8"));
+					    }
+			});
+		/*rspawn1(req.body.TSIDs, req.body.uniqueID, req.body.language).then(reso => {
 			var path2 = path.join(path1, "processCode.txt")
 			fs.writeFile(path2, reso.toString(), (err) => {
+				  if (err)
+					    console.log(err);
+				  else {
+					      console.log("File written successfully\n");
+					      console.log("The written has the following contents:");
+					      console.log(fs.readFileSync(path2, "utf8"));
+					    }
+			});
+			fs.writeFile('TSIDs.txt', JSON.stringify(req.body.TSIDs), (err) => {
 				  if (err)
 					    console.log(err);
 				  else {
@@ -119,7 +137,7 @@ app.post('/lipds', function(req, res) {
 			if (reso == 0 && req.body.language == "Python"){
 				pickleEm(path1)
 			}
-		});
+		});*/
 	}
 
 });
