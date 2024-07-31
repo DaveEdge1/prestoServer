4efe8f588c04c360dc8e311610f7deb1d4e31f27
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-07-31 12:54:53 -0500

    Update writeQueryForm.js

--- query/writeQueryForm.js
+++ query/writeQueryForm.js
@@ -407,7 +407,7 @@ const makeRect = function (recon){
     	const ret = YAML.parse(s)
 	const coords = ret['Location Filters']['coordBox']['defaultBounds']
 	console.log('makeRect() coords: ' + coords)
-	return String.raw`let rect = L.rectangle([[`+coords[0]+`, `+coords[1]+`], [`+coords[2]+`, `+coords[3]+`]], { color: "#ff7800", opacity:0.1, fill: "#ff7800", fillOpacity:0.1, draggable: true });` + `\n`
+	return String.raw`let rect = L.rectangle([[`+coords[0]+`, `+coords[2]+`], [`+coords[1]+`, `+coords[3]+`]], { color: "#ff7800", opacity:0.1, fill: "#ff7800", fillOpacity:0.1, draggable: true });` + `\n`
 }
 
 const groupHTML = function(key, desc) {
