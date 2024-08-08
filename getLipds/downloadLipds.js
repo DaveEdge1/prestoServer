92a110828287f85e79e537d7fceae2cbb8a6ffdf
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-08-08 15:33:52 -0700

    Update downloadLipds.js

--- getLipds/downloadLipds.js
+++ getLipds/downloadLipds.js
@@ -42,6 +42,7 @@ var rspawn1 = function (TSIDs, uniqueID, language){
 	return new Promise((resolve, reject) => {
 			var rspawn = child_process.spawn(r_comm,[args]);
 
+			/*
 			rspawn.stdout.on('data', function (data) {
 		  		console.log(data.toString());
 			});
@@ -55,7 +56,7 @@ var rspawn1 = function (TSIDs, uniqueID, language){
 					rspawn.kill('SIGTERM');
 				}
 			});
-
+			*/
 			rspawn.on('close', function (code) {
 		  		console.log('child process exited with code ' + code);
 		  		resolve(code)
