20252a690df0b3d043cb327ae6e3acd841dce440
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-08-29 12:01:33 -0700

    Update slider.js

--- jsonEditor/public/slider.js
+++ jsonEditor/public/slider.js
@@ -103,6 +103,24 @@ const advAll = document.querySelectorAll('.form-group-advanced');
 
 var val1 = 0;
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+	          coll[i].addEventListener("click", function() {
+			                        this.classList.toggle("active");
+			                        var content = this.nextElementSibling;
+			                        if (content.style.display === "block") {
+							                                    content.style.display = "none";
+							                                  } else {
+												                                                  content.style.display = "block";
+												                                                }
+			                      });
+	        if (i==0) {
+			                coll[i].click()
+			        }
+}
+
 function showAdv() {
 	//document.getElementById("advCheck").value = 0;
 	//document.getElementById("advancedShow").innerHTML = val1
