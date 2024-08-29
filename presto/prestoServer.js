4f5e0305ed39e201e10d85992a00562a9280a6c4
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-08-29 15:59:43 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -270,7 +270,7 @@ emailHTML = function (uniqueID, destURL, configLoc, recon) {
 	var text1 = '<p>Thank you for using PReSto! Use the URL below to download the results of your custom ' + reconParams(recon).title + '. This link will expire after 7 days.</p>'
 	text1 = text1
 		+ '<br>'
-		+ `'` + vizURL(configLoc) + `'` +
+		+ vizURL(configLoc)
 		+ '<a href="' + destURL + '" download>Copy and paste this URL into a new browser window to DOWNLOAD your results</a>'
 		+ '<br>'
 	        + '<a href="' + indexURL + '" download>Copy and paste this URL into a new browser window to BROWSE ALL files created</a>'
