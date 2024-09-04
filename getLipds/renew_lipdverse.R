b34f30b892fcddfc156f90aeeeb4290b895dcf6d
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-04 13:52:19 -0700

    Update renew_lipdverse.R

--- getLipds/renew_lipdverse.R
+++ getLipds/renew_lipdverse.R
@@ -12,6 +12,7 @@ save(tts, file="/root/presto/getLipds/lipdverse_tts.RData")
 #update the available downloads based on TSID lists
 setwd("/root/presto/userRecons")
 a1 <- list.files(pattern = "TSIDs.json$", recursive = TRUE)
+a1 <- vapply(strsplit(a1, "/", fixed = TRUE), "[", "", 1)
 if length(a1 > 0){
   a1 <- lapply(a1, function(x) tools::md5sum(x))
   df1 <- data.frame("location" = names(unlist(a1)), "md5" = unname(unlist(a1)))
