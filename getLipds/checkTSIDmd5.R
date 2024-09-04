a4376680fa9c16a944bc94017c99e17790556f7a
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-04 13:57:45 -0700

    Update checkTSIDmd5.R

--- getLipds/checkTSIDmd5.R
+++ getLipds/checkTSIDmd5.R
@@ -1,9 +1,11 @@
+args = commandArgs(trailingOnly=TRUE)
+
 existing <- read.csv("/root/presto/getLipds/TSIDmd5.csv")
-TSIDs.loc <- file.path(arg, "TSIDs.json")
+TSIDs.loc <- file.path(args[1], "TSIDs.json")
 if (length(existing) > 0){
   a1 <- tools::md5sum(TSIDs.loc)
   md5Index <- which(existing$md5 == a1)[1]
   if (md5Index > 0){
-    write(existing$location[md5Index], file = file.path(arg, "pointer.txt"))
+    write(existing$location[md5Index], file = file.path(args[1], "pointer.txt"))
     }
   }
