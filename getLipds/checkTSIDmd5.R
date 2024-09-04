acb687b94d8b6666dc7937a11518d6ad97ae3c1f
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-04 11:15:33 -0700

    Update checkTSIDmd5.R

--- getLipds/checkTSIDmd5.R
+++ getLipds/checkTSIDmd5.R
@@ -1,6 +1,6 @@
 existing <- read.csv("/root/presto/getLipds/TSIDmd5.csv")
 if (length(existing) > 0){
   a1 <- tools::md5sum(arg)
-  md5Index <- which(existing == a1)
+  md5Index <- which(existing$md5 == a1)
   if (md5Index > 0){
     
