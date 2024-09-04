c0747b8718acaed194a9cf8b6fe051ce0eeb6292
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-04 10:53:54 -0700

    Update renew_lipdverse.R

--- getLipds/renew_lipdverse.R
+++ getLipds/renew_lipdverse.R
@@ -1,4 +1,6 @@
 library(lipdR)
+
+#update the lipdverse tts file for TSID-based filtering
 load("/root/presto/getLipds/lipdverse.RData")
 rm(D)
 gc()
@@ -6,3 +8,9 @@ tts <- as.lipdTsTibble(TS)
 rm(TS)
 gc()
 save(tts, file="/root/presto/getLipds/lipdverse_tts.RData")
+
+#update the available downloads based on TSID lists
+setwd("/root/presto/userRecons")
+a1 <- list.files(pattern = "TSIDs.json$", recursive = TRUE)
+a1 <- lapply(a1, function(x) tools::md5sum(x))
+write.csv(unname(unlist(a1)), "/root/presto/getLipds/TSIDmd5.csv")
