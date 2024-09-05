6a5183f329a5a23d2dbe2e8a6a479d59a65d20f2
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-05 14:14:45 -0700

    Update downloadLipds.js

--- getLipds/downloadLipds.js
+++ getLipds/downloadLipds.js
@@ -26,8 +26,8 @@ var checkmd5 = async function (uniqueID){
 	var path9 = path.join(__dirname, '/checkTSIDmd5.R')
 	var path9999 = path.join(__dirname, '../userRecons', uniqueID, 'TSIDs,json')
 	const exists1 = await checkFileExistsSync(path9999)
-	console.log("exists1: " + exists1)
 	if (!exists1){
+		console.log("no TSID.json file in directory: " + uniqueID)
 		process.exit(1);
 	}
 	var args2 = '--vanilla ' + path9 + ' ' + path99;
