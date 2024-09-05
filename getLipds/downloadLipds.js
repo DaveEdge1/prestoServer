3d607a5b467de84a5c1a1d15dd761b5463af9da6
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-05 15:08:52 -0700

    Update downloadLipds.js

--- getLipds/downloadLipds.js
+++ getLipds/downloadLipds.js
@@ -16,11 +16,11 @@ async function routeExistingLipds(uniqueID){
 	if (checkFileExistsSync(path99)){
 		const s1 = fs.readFileSync(path99,'utf8');
 		var bashText2 = 'ln -s ' + s1 + '/lipd.pkl ' + root1 + '/lipd.pkl'
-		var { stdout, stderr } = exec(bashText2);
+		var { stdout, stderr } = child_process.exec(bashText2);
 		stdout.pipe(fs.createWriteStream(dirname+'create_pkl_shortcut_stdout.txt'));
 	  	stderr.pipe(fs.createWriteStream(dirname+'create_pkl_shortcut_stderr.txt'));
 		var bashText3 = 'ln -s ' + s1 + '/lipd.rds ' + root1 + '/lipd.rds'
-		var { stdout, stderr } = exec(bashText3);
+		var { stdout, stderr } = child_process.exec(bashText3);
 		stdout.pipe(fs.createWriteStream(dirname+'create_rds_shortcut_stdout.txt'));
 	  	stderr.pipe(fs.createWriteStream(dirname+'create_rds_shortcut_stderr.txt'));
 		return true
