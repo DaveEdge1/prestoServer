3781d80bd120c104ba7fafdacb785f41d03b63cf
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-05 13:28:20 -0700

    Update checkTSIDmd5.R

--- getLipds/checkTSIDmd5.R
+++ getLipds/checkTSIDmd5.R
@@ -5,7 +5,7 @@ TSIDs.loc <- file.path(args[1], "TSIDs.json")
 if (length(existing) > 0){
   a1 <- tools::md5sum(TSIDs.loc)
   md5Index <- which(existing$md5 == a1)[1]
-  if (md5Index > 0){
+  if (length(md5Index) > 0){
     write(existing$location[md5Index], file = file.path(args[1], "pointer.txt"))
-    }
   }
+}
