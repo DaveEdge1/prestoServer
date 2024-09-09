59b1087d3be822597d4fa86c3fbc53c8f264e1f4
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-09 15:40:14 -0700

    Update prestoServer.js

--- presto/prestoServer.js
+++ presto/prestoServer.js
@@ -487,7 +487,7 @@ runRecon = async function(uniqueID, user, domain, recon, language) {
   
 prestoStartHtml = function (uniqueID, user, domain, recon, language) {
 	var bashText2 = 'node /root/presto/presto/prestoGo.js ' + uniqueID + ' ' + user + ' ' + domain + ' ' + recon + ' ' + language
-	console.log(execSync(bashText2).toString())
+	console.log(shelljs.exec(bashText2).stdout)
 	return('Starting your custom PReSto ' + reconParams(recon).title + ', ID: '+ uniqueID +'<br /><br />' 
 		 + "WARNING: Using your browser's 'back' button will overwrite your previous submission<br /><br />"  
 		 + '<a href=' + reconParams(recon).github + 'target="_blank">visit the ' + reconParams(recon).title + ' webpage</a><br /><br />' 
