8e2f123094ea4eca73ac8583535390cd5deda6aa
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-09-09 13:56:37 -0700

    Update downloadLipds.js

--- getLipds/downloadLipds.js
+++ getLipds/downloadLipds.js
@@ -17,6 +17,7 @@ async function routeExistingLipds(uniqueID){
 	var path99 = path.join(root1, '/pointer.txt')
 	if (checkFileExistsSync(path99)){
 		const s1 = fs.readFileSync(path99,'utf8');
+		console.log("s1: " + s1)
 		var origpkl = root0 + s1 + '/lipd.pkl'
 		var origrds = root0 + s1 + '/lipd.rds'
 		var bashText2 = 'ln -s ' + origpkl + ' ' + root1
