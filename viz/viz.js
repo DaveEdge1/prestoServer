729656973c48f147b2d82c6fce80d21b0265fc29
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-12-11 12:49:52 -0700

    Update viz.js

--- viz/viz.js
+++ viz/viz.js
@@ -6,7 +6,7 @@ var path = require('path')
 var app = express();
 
 setPage = function(dir1){
-	app.use(express.static(dir1 + 'assets'));
+	app.use(express.static(dir1 + 'viz/assets'));
 	var html_name = dir1 + 'visualizer.html';
 	return html_name;
 }
