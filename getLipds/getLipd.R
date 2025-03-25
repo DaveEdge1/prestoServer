fac7042566cbc7d8b61574908dedbf435348da90
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2025-03-25 12:51:53 -0700

    Create getLipd.R

---
+++ getLipds/getLipd.R
@@ -0,0 +1,38 @@
+args = commandArgs(trailingOnly=TRUE)
+library(lipdR)
+print(paste0("args: ", args))
+if (length(args) != 3){
+	stop("TSIDs, uniqueID, and language required")
+}
+TSIDs <- unlist(strsplit(args[1], split = ","))
+if (length(TSIDs) < 1){
+	stop("Requires at least 1 TSId")
+}
+
+qt <- read.csv("/root/presto/getLipds/lipdverseQuery.csv")
+TSIndex <- which(qt$paleoData_TSid %in% TSIDs)
+
+if(length(TSIndex)==0){
+	print(paste0("Supplied TDids: ", TSIDs))
+	stop("Error: Some of the listed TSids not located in query table")
+}
+
+dsPick <- unique(qt$datasetId[TSIndex])
+print(paste0("datasetIDs: ", dsPick))
+
+load("/root/presto/getLipds/lipdverse_tts.RData")
+tts <- tts[tts$datasetId %in% dsPick,]
+print(paste0("dim(tts): ", dim(tts)))
+print(paste0("unique datasets: ", length(unique(tts$datasetId))))
+
+if (length(dsPick) == 1){
+	D <- lipdR::as.lipd(tts)
+} else {
+	D <- lipdR::as.multiLipd(tts)
+}
+
+
+destPath <- file.path(args[2], "lipd.rds") 
+saveRDS(D, destPath)
+writeLipd(D, args[2])
+
