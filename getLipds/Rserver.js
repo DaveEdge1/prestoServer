6e220eaf5ed22cd3d58dbef2f51cc4c13e3c9737
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2025-03-26 14:38:22 -0700

    Update Rserver.js

--- getLipds/Rserver.js
+++ getLipds/Rserver.js
@@ -17,12 +17,6 @@ app.use(bodyParser.json({
 }));
 app.use(cors({origin: 'http://143.198.98.66:86'}));
 
-var child_process = require('child_process');
-var file_path = "getLipd.R";
-var r_comm = '/usr/bin/Rscript';
-let path1 = '';
-//var args = '--vanilla ' + file_path + ' ' + process.argv[2] + ' ' + process.argv[3];
-
 async function createDirectory(TSIDs, uniqueID, dirPath) {
   try {
     fs.mkdirSync(dirPath);
@@ -44,68 +38,6 @@ async function createDirectory(TSIDs, uniqueID, dirPath) {
 	     return 400;
     }
   }
-}
-
-var rspawn1 = function (TSIDs, uniqueID, language){
-	path1 = path.join(__dirname, '../userRecons', uniqueID)
-	if (language == "Python"){
-		var path3 = path.join(path1, "lipd.pkl")
-		fs.writeFile(path3, " ", (err) => {
-			                                  if (err)
-				                                            console.log(err);
-			                                  else {
-								                                                console.log("Blank lipd.pkl written successfully\n");
-								                                              }
-			                        });
-	}
-	var args = '--vanilla ' + file_path + ' ' + TSIDs + ' ' + path1 + ' ' + language;
-	return new Promise((resolve, reject) => {
-			var rspawn = child_process.spawn(r_comm,[args]);
-
-			rspawn.stdout.on('data', function (data) {
-		  		console.log(data.toString());
-			});
-
-			rspawn.stderr.on('data', function (data) {
-		  		console.log('stderr: ' + data);
-				console.log(data.toString().search("error"));
-				console.log(rspawn.connected);
-				if ((data.toString().search("error") != -1) ) {
-					console.log('process has been killed - "error" keyword found in stderr!');
-					rspawn.kill('SIGTERM');
-				}
-			});
-
-			rspawn.on('close', function (code) {
-		  		console.log('child process exited with code ' + code);
-		  		resolve(code)
-			});
-	});
-	
-}
-
-pickleEm = function(path1){
-	                                console.log("launching lipd pickler")
-	                                var dockerComm = "docker run -v " + path1 +":/output -v " + path1 + "/lipd.pkl:/lipd.pkl davidedge/lipd_webapps:lipdPickler"
-	                                var dockerspawn = child_process.exec(dockerComm);
-	                                                        dockerspawn.stdout.on('data', function (data) {
-									                                                                                                console.log(data.toString());
-									                                                                                        });
-
-	                                                        dockerspawn.stderr.on('data', function (data) {
-									                                                                                                console.log('stderr: ' + data);
-									                                                                                                console.log(data.toString().search("error"));
-									                                                                                                console.log(dockerspawn.connected);
-									                                                                                                if ((data.toString().search("error") != -1) ) {
-																						                                                                                                                                                console.log('process has been killed - "error" keyword found in stderr!');
-																						                                                                                                                                                dockerspawn.kill('SIGTERM');
-																						                                                                                                                                        }
-									                                                                                        });
-
-	                                                        dockerspawn.on('close', function (code) {
-									                                                                                                console.log('child process exited with code ' + code);
-									                                                                                                return(code)
-									                                                                                        });
 }
 
 app.post('/lipds', function(req, res) {
