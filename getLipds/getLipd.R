a186f1529739fa12ca34bbf8764fd3ec082cf33b
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2025-03-31 14:42:43 -0700

    Update getLipd.R

--- getLipds/getLipd.R
+++ getLipds/getLipd.R
@@ -34,6 +34,7 @@ filtered_qt <- qt[qt$paleoData_TSid %in% tsPick,]
 #if we only have year, convert to age
 for (ii in dsPick){
 	this.dataset <- filtered_qt[qt$datasetId == ii,]
+	print(paste0("unique variableNames in this dataset: ", unique(this.dataset$paleoData_variableName)))
 	if ("age" %in% this.dataset$paleoData_variableName){
 		print(paste0(ii, " has age"))
 	} else if ("year" %in% this.dataset$paleoData_variableName){
