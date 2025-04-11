5aebff6755f8fa0c133dc1b03b5069d76c7bd762
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2025-04-11 16:24:12 -0700

    Update grabMeta.R

--- getLipds/grabMeta.R
+++ getLipds/grabMeta.R
@@ -1,3 +1,3 @@
 print("updating compilation metadata")
 compilationMetadata <- readLines("https://lipdverse.org/lipdverse/compilationMetadata.json")
-write(compilationMetadata, "C:/users/dce25/Downloads/compilationMetadata.json")
+write(compilationMetadata, "/root/presto/query/public/compilationMetadata.json")
