9e81cf35008dc9503acb95221777fbef72c3e9e8
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2025-04-18 10:54:09 -0700

    Update Rserver.js

--- getLipds/Rserver.js
+++ getLipds/Rserver.js
@@ -40,34 +40,77 @@ async function createDirectory(TSIDs, uniqueID, dirPath) {
   }
 }
 
+function parseLipdSource(bodyIn) {
+	if ("TSIDs" in bodyIn) {
+	  return 'TSIDs'
+	  console.log(`Writing TSIDs`);
+	} else if ("archivedComp" in bodyIn) {
+	  return 'archivedComp'
+	  console.log(`Using Archived Compilation`);
+	} else {
+	  return NULL
+	  console.log(`No TSIDs or archivedComp found!`);
+	}
+}
+
 app.post('/lipds', function(req, res) {
 	var dir1 = '/root/presto/userRecons/' + req.body.uniqueID + '_' + req.body.recon
-	createDirectory(req.body.TSIDs, req.body.uniqueID, dir1).then(status => {
-	    console.log('Final status:', status);
-	    res.sendStatus(status)
-		
-		if (status == 200){
-			var path0 = path.join(dir1, 'TSIDs.json')
-			var fullJSON = `{"TSIDs":` + JSON.stringify(req.body.TSIDs) + `}`
-			fs.writeFile(path0, fullJSON, (err) => {
-				  if (err)
-					    console.log(err);
-				  else {
-					      console.log("File written successfully at: " + path0);
-					    }
-			});
-	
-		} else {
-			var path0 = path.join(dir1, 'TSIDs_err.txt')
-			fs.writeFile(path0, "Rserver error! TSIDs not written.", (err) => {
-				  if (err)
-					    console.log(err);
-				  else {
-					      console.log("File written successfully at: " + path0);
-					    }
-			});
+
+	const lipdSource = parseLipdSource(req.body)
+
+	if ("TSIDs" in req.body){
+		createDirectory(req.body.TSIDs, req.body.uniqueID, dir1).then(status => {
+		    console.log('Final status:', status);
+		    res.sendStatus(status)
+			if (status == 200){
+				var path0 = path.join(dir1, 'TSIDs.json')
+				var fullJSON = `{"TSIDs":` + JSON.stringify(req.body.TSIDs) + `}`
+				fs.writeFile(path0, fullJSON, (err) => {
+					  if (err)
+						    console.log(err);
+					  else {
+						      console.log("File written successfully at: " + path0);
+						    }
+				});
+			} else {
+				var path0 = path.join(dir1, 'TSIDs_err.txt')
+				fs.writeFile(path0, "Rserver error! TSIDs not written.", (err) => {
+					  if (err)
+						    console.log(err);
+					  else {
+						      console.log("File written successfully at: " + path0);
+						}
+				});
+			}
+		});
+	} else if ("archivedComp" in req.body){
+		createDirectory2(req.body.archivedComp, req.body.uniqueID, dir1).then(status => {
+		    console.log('Final status:', status);
+		    res.sendStatus(status)
+			if (status == 200){
+				var path0 = path.join(dir1, 'archivedComp.txt')
+				fs.writeFile(path0, req.body.archivedComp, (err) => {
+					  if (err)
+						    console.log(err);
+					  else {
+						      console.log("File written successfully at: " + path0);
+						    }
+				});
+			} else {
+				var path0 = path.join(dir1, 'archivedComp_Err.txt')
+				fs.writeFile(path0, "Rserver error! archivedComp not written.", (err) => {
+					  if (err)
+						    console.log(err);
+					  else {
+						      console.log("File written successfully at: " + path0);
+						}
+				});
+			}
+		});
+	} else {
+			console.log("Couldn't find TSIDs or archivedComp in POST from query")
 		}
-	});
+		
 });
 
 app.listen(PORT, function () {
