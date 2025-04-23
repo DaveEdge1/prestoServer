c93d0f993e531e7bf3a74db23bf4d548bcc33399
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2025-04-23 15:14:44 -0700

    Update writeTTS.R

--- getLipds/writeTTS.R
+++ getLipds/writeTTS.R
@@ -1,5 +1,14 @@
 args = commandArgs(trailingOnly=TRUE)
 library(lipdR)
-load(args[1])
+path2RData <- paste0(args[1], "/lipd.RData")
+load(path2RData)
 D <- structure(D, class = c("multi_lipd", class(list())))
 tts <- as.lipdTsTibble(D)
+
+print("write filtered tts")		 
+destPaths2 <- file.path(args[1], "lipd_tts.rds") 
+saveRDS(tts, destPaths2)
+			 
+print("write filtered multi-lipd") 
+destPaths3 <- file.path(args[1], "lipd.rds") 
+saveRDS(D, destPaths3)
