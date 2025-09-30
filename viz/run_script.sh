06a1dede2a0dd75c3445c2cf0fca2c71a91f60cb
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2025-09-30 11:40:39 -0700

    Update run_script.sh

--- viz/run_script.sh
+++ viz/run_script.sh
@@ -21,9 +21,9 @@ echo "=== Processing reconstruction ==="
 
 source /root/miniconda3/etc/profile.d/conda.sh &&
 conda activate presto_env &&
-python -u /root/presto/viz/1_format_data_daholocene_graphem.py $data_dir 2>&1 | tee -a "$output_dir/viz/1_format_data_full.log"; echo "1_format_data exit code: $? Memory: $(free -h | grep Mem)" >> "$output_dir/viz/1_format_data_full.log" &&
-python -u /root/presto/viz/2_make_maps_and_ts.py $data_dir $output_dir 2>&1 | tee -a "$output_dir/viz/2_make_maps_full.log"; echo "2_make_maps exit code: $? Memory: $(free -h | grep Mem)" >> "$output_dir/viz/2_make_maps_full.log" &&
-python -u /root/presto/viz/3_make_html_file.py $data_dir $output_dir $web_data_dir 2>&1 | tee -a "$output_dir/viz/3_make_html_full.log"; echo "3_make_html exit code: $? Memory: $(free -h | grep Mem)" >> "$output_dir/viz/3_make_html_full.log" &&
+python -u /root/presto/viz/1_format_data_daholocene_graphem.py $data_dir 2>&1 | tee -a "$output_dirviz/1_format_data_full.log"; echo "1_format_data exit code: $? Memory: $(free -h | grep Mem)" >> "$output_dirviz/1_format_data_full.log" &&
+python -u /root/presto/viz/2_make_maps_and_ts.py $data_dir $output_dir 2>&1 | tee -a "$output_dirviz/2_make_maps_full.log"; echo "2_make_maps exit code: $? Memory: $(free -h | grep Mem)" >> "$output_dirviz/2_make_maps_full.log" &&
+python -u /root/presto/viz/3_make_html_file.py $data_dir $output_dir $web_data_dir 2>&1 | tee -a "$output_dirviz/3_make_html_full.log"; echo "3_make_html exit code: $? Memory: $(free -h | grep Mem)" >> "$output_dirviz/3_make_html_full.log" &&
 
 conda activate base
 
